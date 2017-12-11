3b8ae4cf0abc7f2690b95542f70a2a7be119d13d
Helen <heb264@gmail.com>
2017-12-11 12:19:02 +0800

    update to servers component

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -3,16 +3,20 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-servers', 
   //selector: '[app-servers]' as an attribute
- // selector: '.app-servers' as a class
-  template: `
-    <app-server></app-server>
-    <app-server></app-server>
-  `,
+  // selector: '.app-servers' as a class
+  //template: `
+   // <app-server></app-server>
+   // <app-server></app-server>`
+   templateUrl: './servers.component.html',
   styleUrls: ['./servers.component.css']
 })
 export class ServersComponent implements OnInit {
-
-  constructor() { }
+  allowNewServer = false;
+  constructor() { 
+    setTimeout(()=>{
+      this.allowNewServer = true;
+    },2000);
+  }
 
   ngOnInit() {
   }
